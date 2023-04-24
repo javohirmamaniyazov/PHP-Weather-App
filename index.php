0b2ebe7559898a505f060888f5657c385c38af29
Javohir Mamaniyazov <javohirmamaniyazov@gmail.com>
2023-04-24 16:02:41 +0500

    finished simple weather app

--- index.php
+++ index.php
@@ -10,10 +10,11 @@ if (isset($_POST['submit'])) {
     $result = curl_exec($ch);
     curl_close($ch);
     $result = json_decode($result, true);
-    if ($result == 200) {
+    if ($result['cod'] == 200) {
         $status = "yes";
     } else {
         $msg = $result['message'];
+        echo "<script>alert('City not found. Please enter a valid city name.')</script>";
     }
 }
 ?>
@@ -24,145 +25,178 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <title>Weather Card</title>
     <style>
-        @import url(https://fonts.googleapis.com/css?family=Poiret+One);
-        @import url(https://cdnjs.cloudflare.com/ajax/libs/weather-icons/2.0.9/css/weather-icons.min.css);
-
         body {
-            background-color: cadetblue;
-            font-family: Poiret One;
-        }
-
-        .widget {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            display: flex;
-            height: 300px;
-            width: 600px;
-            transform: translate(-50%, -50%);
-            flex-wrap: wrap;
-            cursor: pointer;
-            border-radius: 20px;
-            box-shadow: 0 27px 55px 0 rgba(0, 0, 0, 0.3), 0 17px 17px 0 rgba(0, 0, 0, 0.15);
-        }
-
-        .widget .weatherIcon {
-            flex: 1 100%;
-            height: 60%;
-            border-top-left-radius: 20px;
-            border-top-right-radius: 20px;
-            background: #FAFAFA;
-            font-family: weathericons;
-            display: flex;
-            align-items: center;
-            justify-content: space-around;
-            font-size: 100px;
-        }
-
-        .widget .weatherIcon i {
-            padding-top: 30px;
-        }
-
-        .widget .weatherInfo {
-            flex: 0 0 70%;
-            height: 40%;
-            background: darkslategray;
-            border-bottom-left-radius: 20px;
-            display: flex;
-            align-items: center;
-            color: white;
-        }
-
-        .widget .weatherInfo .temperature {
-            flex: 0 0 40%;
-            width: 100%;
-            font-size: 65px;
-            display: flex;
-            justify-content: space-around;
-        }
-
-        .widget .weatherInfo .description {
-            flex: 0 60%;
-            display: flex;
-            flex-direction: column;
-            width: 100%;
-            height: 100%;
-            justify-content: center;
-            margin-left: -15px;
-        }
-
-        .widget .weatherInfo .description .weatherCondition {
-            text-transform: uppercase;
-            font-size: 35px;
-            font-weight: 100;
-        }
-
-        .widget .weatherInfo .description .place {
-            font-size: 15px;
-        }
-
-        .widget .date {
-            flex: 0 0 30%;
-            height: 40%;
-            background: #70C1B3;
-            border-bottom-right-radius: 20px;
-            display: flex;
-            justify-content: space-around;
-            align-items: center;
-            color: white;
-            font-size: 30px;
-            font-weight: 800;
-        }
-
-        p {
-            position: fixed;
-            bottom: 0%;
-            right: 2%;
-        }
-
-        p a {
-            text-decoration: none;
-            color: #E4D6A7;
-            font-size: 10px;
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
         }
 
         .form {
-            position: absolute;
-            top: 42%;
-            left: 50%;
-            display: flex;
-            height: 300px;
-            width: 600px;
-            transform: translate(-50%, -50%);
+            /* margin-bottom: 10px; */
+            width: 100%;
+            text-align: center;
         }
 
         .text {
-            width: 80%;
-            padding: 10px
+
+            width: 60%;
+            border-radius: 5px;
+            border: none;
+            /* outline: none; */
+            margin-bottom: 10px;
+            margin-top: 0px;
+            height: 37px;
         }
 
         .submit {
-            height: 39px;
-            width: 100px;
-            border: 0px;
+            border-radius: 5px;
+            border: none;
+            background-color: #0077be;
+            color: #fff;
+            margin-top: 0px;
         }
 
-        .mr45 {
-            margin-right: 45px;
+        .submit:hover {
+            background-color: #005b8e;
+        }
+
+        .widget {
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+            max-width: 400px;
+            margin: 0 auto;
+            margin-top: 40px;
+        }
+
+        .weatherIcon {
+            float: left;
+            margin-right: 20px;
+        }
+
+        .weatherIcon img {
+            width: 100px;
+            height: 100px;
+        }
+
+        .weatherInfo {
+            overflow: hidden;
+        }
+
+        .temperature {
+            font-size: 48px;
+            font-weight: bold;
+            color: #0077be;
+        }
+
+        .description {
+            margin-top: 20px;
+            font-size: 24px;
+        }
+
+        .weatherCondition {
+            font-weight: bold;
+            text-transform: capitalize;
+        }
+
+        .place {
+            color: #666;
+            text-transform: capitalize;
+        }
+
+        .date {
+            margin-top: 20px;
+            text-align: right;
+            font-size: 18px;
+            color: #666;
+        }
+
+
+        nav {
+            background-color: #0077be;
+            padding: 10px;
+        }
+
+        nav ul {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        nav li {
+            color: #fff;
+
+        }
+
+        nav a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        .logo {
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        .text {
+            padding: 10px;
+            border-radius: 5px;
+            border: none;
+            outline: none;
+            margin-right: 0px;
+
+        }
+
+        .submit {
+            padding: 10px;
+            border-radius: 5px;
+            border: none;
+            background-color: #fff;
+            color: #0077be;
+            cursor: pointer;
+            outline: none;
+
+        }
+
+        .submit:hover {
+            background-color: #eee;
+        }
+
+        .search_form {
+            margin-top: 10px;
+            margin-bottom: 0;
+            width: 500px;
+            font-size: 20px;
+        }
+        .log{
+            margin-left: 50px;
         }
     </style>
 </head>
 
+
 <body>
     <div class="form">
-        <form style="width:100%;" method="post">
-            <input type="text" class="text" placeholder="Enter city name" name="city" value="" />
-            <input type="submit" value="Submit" class="submit" name="submit" />
-            <?php echo $msg; ?>
-        </form>
+        <nav>
+            <ul>
+                <li class="logo"><a href="/" class="log">Weather App</a></li>
+                <li>
+                    <form class="search_form" method="post">
+                        <input type="text" class="text" placeholder="Enter city name" name="city" value="" />
+                        <button type="submit" class="submit" name="submit">🔍</button>
+                    </form>
+                </li>
+            </ul>
+        </nav>
     </div>
 
-    
+
+
     <?php if ($status == "yes") { ?>
         <article class="widget">
             <div class="weatherIcon">
@@ -170,10 +204,10 @@ if (isset($_POST['submit'])) {
             </div>
             <div class="weatherInfo">
                 <div class="temperature">
-                    <span><?php echo round($result['main']['temp'] - 273.15); ?>°</span>
+                    <span><?php echo round($result['main']['temp'] - 273.15); ?>°C </span>
                 </div>
                 <div class="description mr45">
-                    <div class="weatherCondition"><?php echo $result['weather'][0]['main']; ?></div>
+                <div class="weatherCondition"><?php echo $result['weather'][0]['main']; ?></div>
                     <div class="place"><?php echo $result['name']; ?></div>
                 </div>
                 <div class="description">
